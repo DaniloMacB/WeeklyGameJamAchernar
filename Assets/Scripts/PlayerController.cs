161ff081f3d693e540f4d881570bb15991c15436
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2018-09-28 18:12:08 -0300

    Dash e nova movimentação

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,63 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
+	public CharacterController2D controller;
 	private Rigidbody2D rgbd;
 
-	public float speed;
-	public float jumpForce;
+	public float runSpeed;
+	public float dashSpeed;
 
-	private float movX;
+	public enum dir { left, right };
+	public dir direcaoAtual;
 
-	// Use this for initialization
-	void Awake () 
-	{
+	bool jump = false;
+	bool crouch = false;
+	bool dash = false;
+
+	float horizontalMove = 0f;
+
+	void Awake(){
+		direcaoAtual = dir.right;
 		rgbd = GetComponent<Rigidbody2D> ();
 	}
 
-
 	void Update(){
-		movX = Input.GetAxisRaw ("Horizontal");
+
+		if(Input.GetAxisRaw ("Horizontal") < 0){
+			direcaoAtual = dir.left;
+		}
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
+			direcaoAtual = dir.right;
+		}
+
+		horizontalMove = Input.GetAxisRaw ("Horizontal") * runSpeed;
 
 		if (Input.GetButtonDown ("Jump")) {
-			rgbd.velocity = Vector2.zero;
-			rgbd.AddForce (new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+			jump = true;
 		}
+
+		if (Input.GetButtonDown ("Crouch")) {
+			crouch = true;
+		} else if (Input.GetButtonUp ("Crouch")) {
+			crouch = false;
+		}
+
+		if (Input.GetButtonDown ("Dash")) {
+			if(direcaoAtual == dir.left)
+				rgbd.AddForce (Vector2.left * dashSpeed * 10f);
+			else
+				rgbd.AddForce (-Vector2.left * dashSpeed * 10f);
+		}
+
 	}
-		
+
 	void FixedUpdate(){
-		rgbd.velocity = new Vector2(movX * speed, rgbd.velocity.y);
+		controller.Move (horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+
 	}
 
 }
