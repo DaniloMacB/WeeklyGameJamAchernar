8ba466fe24502bef057395b4e7bb28f78d2a40bc
Unknown <danilo.bakun@gmail.com>
2018-09-28 20:08:22 -0300

    pause simples

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	[SerializeField]
+	bool isPaused = false;
+	
+	void Awake(){
+		
+	}
+	
+	void Start(){
+		
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			GamePause ();
+		}
+	}
+
+	public void GamePause(){
+		isPaused = !isPaused;
+		if (isPaused) {
+			Time.timeScale = 0f;
+		} 
+		else 
+		{
+			Time.timeScale = 1f;
+		}
+	}
+}
