646f18d71b5ecb788131d7fef3069feeab9be3eb
Unknown <danilo.bakun@gmail.com>
2018-09-30 09:36:50 -0300

    novas cenas + tiles + menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void Play()
+	{
+		SceneManager.LoadScene(1);
+	}
+	
+	public void Quit()
+	{
+		#if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+		#if (UNITY_EDITOR)
+		UnityEditor.EditorApplication.isPlaying = false;
+		#elif (UNITY_STANDALONE) 
+		Application.Quit();
+		#elif (UNITY_WEBGL)
+		Application.OpenURL("about:blank");
+		#endif
+	}
+}
