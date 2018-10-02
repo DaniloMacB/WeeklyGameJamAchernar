76a7c68ac1b75fa904220c53824f90387f0cb5b0
Unknown <danilo.bakun@gmail.com>
2018-10-02 18:12:34 -0300

    portal pro prox lv

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gate : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.transform.tag == "Player")
+		{
+			Application.LoadLevel( "Pasta1" );
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	
 }
