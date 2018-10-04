4f3dd04cecad7c4288de9441cb74290499eda5c4
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2018-10-03 23:47:43 -0300

    Feito tudo do à fazer
    Feito tudo do à fazer

---
+++ Assets/Scripts/HQControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HQControl : MonoBehaviour {
+
+	public GameObject[] hqs;
+
+	int qualTaAtiva = 1;
+
+	void Awake(){
+
+	}
+
+	void Update(){
+		if (Input.GetButtonDown ("Jump")) {
+			if (qualTaAtiva < hqs.Length) {
+				for (int i = 0; i < hqs.Length; i++) {
+					if (i == qualTaAtiva)
+						hqs [i].SetActive (true);
+					else
+						hqs [i].SetActive (false);
+					
+
+				}
+				qualTaAtiva++;
+			} else {
+				SceneManager.LoadScene ((SceneManager.GetActiveScene ().buildIndex + 1));
+			}
+		}
+	}
+}
